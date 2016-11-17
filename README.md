9775c3e9ec5e30239f64ae25e18162c46984f295
vinz <vincentzerbib@gmail.com>
2016-11-18 00:06:35 +0100

    initialisation du projet

---
+++ README.md
@@ -0,0 +1,2 @@
+## Banana-bread
+Web landing page example with Angular 2
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// gulp.task('browser-sync', function() {
+//     bs.init({
+//         server: {
+//             baseDir: "./app"
+//         }
+//     });
+// });
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Other watchers
+})
